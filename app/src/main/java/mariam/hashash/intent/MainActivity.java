55ea83a1fd6e32893b8f133ef6e3b2d88900dae0
sag <dsffsdfsdf@hotmail.com>
2017-10-03 20:13:16 +0300

    Initial commit

---
+++ app/src/main/java/mariam/hashash/intent/MainActivity.java
@@ -0,0 +1,29 @@
+package mariam.hashash.intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void mansaf(View view) {
+       /*
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+         startActivity(i);
+         */
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
+    }
+}
